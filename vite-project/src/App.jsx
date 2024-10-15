e0b6d2dfda6e6c11023d82f3e49c02140697dc01
DonPepino15 <143625105+DonPepino15@users.noreply.github.com>
2024-10-14 21:46:10 -0500

    Iframe_Video

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,23 +1,34 @@
-import React, { useContext } from 'react'
-import Sidebar from './components/Sidebar'
-import Player from './components/Player'
-import Display from './components/Display'
-import { PlayerContext } from './context/PlayerContext'
+import React, { useContext } from 'react';
+import Sidebar from './components/Sidebar';
+import Player from './components/Player';
+import Display from './components/Display';
+import { PlayerContext } from './context/PlayerContext';
 
-const App = () =>{
+const App = () => {
+  const { audioRef, track } = useContext(PlayerContext);
 
-  const {audioRef, track} = useContext(PlayerContext);
-
-  return(
-    <div className='h-screen bg-black'> 
+  return (
+    <div className='h-screen bg-black'>
       <div className='h-[90%] flex'>
-        <Sidebar/>
-        <Display/>
+        <Sidebar />
+        <Display />
+        {/* Reproducción de video con iframe */}
+        <div className='w-full h-full'>
+          <iframe
+            width="100%"
+            height="100%"
+            src={track.videoUrl}
+            title={track.title}
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
       </div>
-      <Player/>
+      <Player />
       <audio ref={audioRef} src={track.file} preload='auto'></audio>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
