674f0585bfc472218effa45e2406902f0c420ce5
DonPepino15 <143625105+DonPepino15@users.noreply.github.com>
2024-10-16 20:28:02 -0500

    Update App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,35 +1,23 @@
-import React, { useContext } from 'react';
-import Sidebar from './components/Sidebar';
-import Player from './components/Player';
-import Display from './components/Display';
-import { PlayerContext } from './context/PlayerContext';
+import React, { useContext } from 'react'
+import Sidebar from './components/Sidebar'
+import Player from './components/Player'
+import Display from './components/Display'
+import { PlayerContext } from './context/PlayerContext'
 
-const App = () => {
-  const { audioRef, track } = useContext(PlayerContext);
+const App = () =>{
 
-  return (
-    <div className='h-screen bg-black'>
+  const {audioRef, track} = useContext(PlayerContext);
+
+  return(
+    <div className='h-screen bg-black'> 
       <div className='h-[90%] flex'>
-        <Sidebar />
-        <Display />
-        {/* Reproducción de video con iframe */}
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/Yq6qkq_TWAM?si=Rm-EPGcuk1tFpSVh" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        <div className='w-full h-full'>
-         <iframe
-            width="100%"
-            height="100%"
-            src={track.videoUrl}  // URL del video
-            title={track.title}
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          ></iframe>
-        </div>
+        <Sidebar/>
+        <Display/>
       </div>
-      <Player />
+      <Player/>
       <audio ref={audioRef} src={track.file} preload='auto'></audio>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
