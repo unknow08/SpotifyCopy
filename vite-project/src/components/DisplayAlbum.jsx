57c0908947501c71792649668a44227333bcc5df
Carlos Rangel <carloseduardo712404@hotmail.com>
2024-10-20 15:17:02 -0500

    Full integracion videoPlayer con BD (3/4) El video carga pero no cambia de video al dar click sobre otro

--- vite-project/src/components/DisplayAlbum.jsx
+++ vite-project/src/components/DisplayAlbum.jsx
@@ -144,7 +144,7 @@ const DisplayAlbum = () => {
                     <hr />
                     {
                         videos.map((video, index) => (
-                            <div key={index} className="flex justify-between items-center p-2 text-[#a7a7a7] hover:bg-[#ffffff2b] cursor-pointer w-full" onClick={() => handleSongClick(item.videoId)}>
+                            <div key={index} className="flex justify-between items-center p-2 text-[#a7a7a7] hover:bg-[#ffffff2b] cursor-pointer w-full" onClick={() => handleSongClick(video.url.split('=').pop())}>
                                 <div className="flex items-center text-white">
                                     <b className="mr-4 text-[#a7a7a7]">{index + 1}</b>
                                     <img className="inline w-10 mr-5" src={video.img} alt="" />
